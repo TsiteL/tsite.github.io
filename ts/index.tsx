4bac2b4b9dd2b7510afcf7f9e5aeec5ea169f7a1
Hit The Target <36575353+tolek-rudnicki@users.noreply.github.com>
2021-05-04 10:30:29 +0200

    change sites

--- ts/index.tsx
+++ ts/index.tsx
@@ -4,13 +4,13 @@
 var light_mode = false;
 // Functions
 function goMain() {
-    location.href = "file:///C:/Users/user/Desktop/Html/Tsite/index.html";
+    location.href = "http://tsitel.github.io/index.html";
 }
 function search() {
 
 }
 function goto_settings() {
-    var set_url = 'file:///C:/Users/user/Desktop/Html/Tsite/settings/index.html';
+    var set_url = 'http://tsitel.github.io/settings/index.html';
     window.open(set_url, '_blank');
 }
 function openNewRoomDialog() {
@@ -30,7 +30,7 @@ function makeNewRoom() {
         rdsc.className += " error"
         return;
     }
-    location.href = "file:///C:/Users/user/Desktop/Html/Tsite/room_design/room.html?id=12345&rname=" + rnam.value;
+    location.href = "http://tsitel.github.io/room_design/room.html?id=12345&rname=" + rnam.value;
 
 }
 
